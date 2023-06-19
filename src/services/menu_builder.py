497c4cc5ab1c1336dbff09a620d8d2445c9f04f5
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2023-06-19 12:50:53 -0300

    4 - Geração dos cardápios

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -26,4 +26,16 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> List[Dict]:
-        pass
+        menu = []
+
+        for dish in self.menu_data.dishes:
+            if not restriction or restriction not in dish.get_restrictions():
+                dish_entry = {
+                    "dish_name": dish.name,
+                    "price": dish.price,
+                    "ingredients": dish.get_ingredients(),
+                    "restrictions": dish.get_restrictions(),
+                }
+                menu.append(dish_entry)
+
+        return menu
